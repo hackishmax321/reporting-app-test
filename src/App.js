1d64ee3daf7c8243ee473e2470993b3cae2b0fde
edirisingheCha <edirisinghe.cha@gmail.com>
2021-07-13 08:21:08 +0530

    chnage ui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+//import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './routes/home';
 import NavBar from './components/containers/navbar/navbar';
